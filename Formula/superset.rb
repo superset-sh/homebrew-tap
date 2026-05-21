b7823a25033ea0933a4939891ca1742b8c7e7e5e
Satya Patel <satyapatel111@gmail.com>
2026-05-21 16:53:54 -0700

    superset 0.2.19

--- Formula/superset.rb
+++ Formula/superset.rb
@@ -1,24 +1,24 @@
 class Superset < Formula
   desc "CLI and host-service for Superset"
   homepage "https://superset.sh"
-  version "0.2.7"
+  version "0.2.19"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/superset-sh/superset/releases/download/cli-v#{version}/superset-darwin-arm64.tar.gz"
-      sha256 "7cf0f033e34528e4b977c8941cf093ddcf85080b58d64902c567b0800f481ebc"
+      sha256 "6ef295cae43f624b985651b855118dd4528b0348f90f4d314d1a103bc3c1cbca"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/superset-sh/superset/releases/download/cli-v#{version}/superset-linux-x64.tar.gz"
-      sha256 "6a3ef5a9bb6c6cb0d4b97ea32f0f91efd06ddb377b80ad2a59610b9ef941ebaa"
+      sha256 "a3ee4229ac6e2f0fac51ecdcf2743504f57f8a6d5451e09161cfef1a131220a1"
     end
     on_arm do
       url "https://github.com/superset-sh/superset/releases/download/cli-v#{version}/superset-linux-arm64.tar.gz"
-      sha256 "5360ba889fab5f2e19fae8eecd020ae720fba48f18ac7c3e34ff72de6c2eb933"
+      sha256 "aa9cef17dd1979603ed15f5326d3a42a1c2cc2e8dd021d68b304e1285bacfe92"
     end
   end
 
